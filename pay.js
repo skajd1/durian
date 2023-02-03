df18611c929aaf6c73df879e4b7f964579fa95e2
skajd1 <woochul102@gmail.com>
2023-02-03 16:34:35 +0900

    pay.ejs Ajax  call 업데이트

--- pay.js
+++ pay.js
@@ -46,17 +46,59 @@ router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         let date = req.query['select-date'];
         let movieid = req.query['movie-id'];
         let sql_moviedetail = "select * from moviedetail; ";
-        // let sql_selected_movie : string = "select title,age, runningTime, poster_src from moviedetail where movieid = ?; "
-        // let params_detail : Array<any> = [movieid]
         let sql_places = "select * from places";
-        // let params_place : Array<any> = [placeid]
+        let sql_table = "select time1, time2, time3, time4, time5 from timetable where placeid = ? and date = STR_TO_DATE(?,'%Y-%m-%d'); ";
+        let params_table = [placeid, date];
         let conn = yield pool.getConnection();
         try {
+            let err;
             let [rows] = yield conn.query(sql_moviedetail + sql_places);
             movielist = rows[0];
             placelist = rows[1];
+            let [time] = yield conn.query(sql_table, params_table);
+            if (!time.length) {
+                err = "선택한 극장 / 날짜에 상영중인 영화가 없습니다. 다른 극장 / 날짜를 선택해주세요.";
+            }
             conn.release();
-            return res.render('pay', { login: true, movielist: movielist, placelist: placelist, placeid: placeid, movieid: movieid, date: date });
+            return res.render('pay', { login: true, movielist: movielist, placelist: placelist, placeid: placeid, movieid: movieid, date: date, err: err });
+        }
+        catch (err) {
+            console.error(err);
+            conn.release();
+        }
+    }
+}));
+// router.post('/', async (req: Request, res: Response) =>{
+//     if(!req.session.isLogined){
+//         return res.send("<script>alert('로그인 후 이용해주세요.');document.location.href='/'</script>")
+//     }
+//     else{
+//         console.log(req.body)
+//         let conn = await pool.getConnection();
+//         try{
+//             conn.release();
+//         } catch(err) {
+//             console.error(err)
+//             conn.release();
+//         }
+//     }
+// })
+router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.session.isLogined) {
+        return res.send("<script>alert('로그인 후 이용해주세요.');document.location.href='/'</script>");
+    }
+    else {
+        let data = req.body;
+        let movieid = data.movieid;
+        let placeid = data.placeid;
+        let date = data.date;
+        let conn = yield pool.getConnection();
+        try {
+            let sql_table = "select time1, time2, time3, time4, time5 from timetable where placeid = ? and date = STR_TO_DATE(?,'%Y-%m-%d');";
+            let params_table = [placeid, date];
+            let [times] = yield conn.query(sql_table, params_table);
+            conn.release();
+            return res.send(times);
         }
         catch (err) {
             console.error(err);
