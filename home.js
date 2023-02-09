feaf80e5641dc3923993ffbb2b34fe994f0c57dd
skajd1 <woochul102@gmail.com>
2023-02-09 15:46:14 +0900

    portnum fixed

--- home.js
+++ home.js
@@ -21,7 +21,7 @@ const session = require('express-session');
 const mysqlStore = require('express-mysql-session')(session);
 const options = {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORTNUM,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- src/admin.ts
+++ src/admin.ts
@@ -13,7 +13,7 @@ type Movie={
 const session = require('express-session');
 const options = {
     host : process.env.DB_HOST,
-    port : process.env.DB_PORTNUM,
+    port : 3306,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,
