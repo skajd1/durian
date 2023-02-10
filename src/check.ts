c7513e9b7e065eb1a33a789cf12579b79b385632
skajd1 <woochul102@gmail.com>
2023-02-10 16:59:01 +0900

    birth vrfc-layout update

--- src/check.ts
+++ src/check.ts
@@ -57,6 +57,7 @@ async function checkDup(id : string)
 //날짜 유효성 검사 TODO
 function checkBirth(year : string) : boolean
 {
+    
     if(year.length != 4 || Number(year) < 1900 || Number(year) >= 2024)
     {
         return false;
